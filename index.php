3fa8e2742f20f074087cc70d31066d9c05cd69df
xspaces <dev.xspaces@gmail.com>
2023-09-29 19:22:53 -0300

    29-09

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <title>XSpaces - Processo de desenvolvimento</title>
+    <link rel="stylesheet" href="css/mdb.min.css" />
+  </head>
+  <body>
+
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap');
+    body {
+        background: black;
+    }
+    #main {
+        width: 100%;
+        min-height: 100vh;
+        color: white;
+        font-family: 'Poppins', sans-serif;
+        padding-top: 2.5rem;
+    }
+
+    .bold {
+        font-weight: 700;
+    }
+
+    .extra-bold {
+        font-weight: 900;
+    }
+    .logo {
+        width: 10rem;
+        height: auto;
+    }
+
+    .h1 {
+        font-size: 2rem;
+    }
+    .h2 {
+        font-size: 1.6rem;
+    }
+    .half-size {
+        max-width: 60%;
+    }
+    .process {
+        padding-left: 2rem;
+        margin-bottom: 4rem;
+        border-left: 4px solid rgba(255,255,255,0.2);
+        transition: .3s ease-in-out;
+    }
+    .process:hover {
+        border-left: 4px solid #e24242;
+        transition: .3s ease-in-out;
+    }
+    p {
+        opacity: .5;
+    }
+    .red-principal {
+        color: #e24242;
+    }
+    .mr-2 {
+        margin-right: 2rem;
+    }
+  </style>
+
+    <div id="main">
+        <div class="container">
+           <div class="mb-5">
+           <img src="img/logo.png" alt="" class="logo mr-2">
+            <img src="img/evoa.png" alt="" class="logo">
+           </div>
+            <!-- <h1 class="h1 extra-bold mb-5"><span class="red-principal">PROCESSO</span> DE DESENVOLVIMENTO</h1> -->
+            <div class="process">
+             <h2 class="bold mb-3 h2">1. ESTRUTURAÇÃO DE EQUIPE</h2>
+             <p class="half-size">Criar uma equipe Scrum composta por membros multifuncionais, incluindo desenvolvedores, designers, testadores e um Product Owner (PO) que represente os interesses do cliente.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">2. BACKLOG DE PRODUTO</h2>
+             <p class="half-size">Criar um Backlog do Produto que liste todos os recursos, funcionalidades e tarefas necessários para o desenvolvimento do MVP. Isso incluirá pesquisa, design, desenvolvimento e testes.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">3. SPRINT PLANNING</h2>
+             <p class="half-size">Realizar reuniões de planejamento de Sprint para definir os objetivos da Sprint atual (geralmente 2-4 semanas) com base no Backlog do Produto. Priorizar as tarefas que são essenciais para o MVP.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">4. SPRINTS</h2>
+             <p class="half-size">Dividir o projeto em várias Sprints, cada uma com um conjunto específico de tarefas a serem concluídas. No final de cada Sprint, deve haver um incremento funcional do MVP.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">5. REUNIÕES DIÁRIAS</h2>
+             <p class="half-size">Realizar reuniões diárias curtas para que a equipe compartilhe atualizações sobre o progresso, discuta obstáculos e ajuste o plano conforme necessário.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">6. REVISÃO DA SPRINT</h2>
+             <p class="half-size">Após o término de cada Sprint, realizar uma reunião de revisão da Sprint com o cliente para demonstrar o que foi realizado e obter feedback imediato.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">7. RETROSPECTIVA DA SPRINT</h2>
+             <p class="half-size">Realizar uma retrospectiva da Sprint com a equipe para identificar o que funcionou bem e o que pode ser melhorado no próximo ciclo.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">8. ADAPTAÇÃO CONTÍNUA</h2>
+             <p class="half-size">Com base no feedback recebido durante as revisões da Sprint e nas retrospectivas, ajustar o Backlog do produto e o plano das próximas Sprints para refletir as mudanças nas prioridades e necessidades do cliente.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">9. ENTREGA DO MVP</h2>
+             <p class="half-size">Continuar a desenvolver e iterar até que o MVP esteja completo e pronto para entrega ao cliente.</p>
+            </div>
+            <div class="process">
+             <h2 class="bold mb-3 h2">10. MONITORAMENTO E MELHORIA</h2>
+             <p class="half-size">Após a entrega do MVP, continuar a acompanhar o uso e a coletar feedback do cliente para fazer melhorias incrementais no produto.</p>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+  </body>
+</html>
